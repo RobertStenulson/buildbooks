e68e48aa5c413afc2545d7b6a84847a72c1cbf2d
Robert Stenulson <61328772+RobertStenulson@users.noreply.github.com>
2023-07-19 19:44:07 -0300

    added login form/style changes

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set up the static middleware to serve files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the view engine and views folder
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+// Simulated user authentication status
+let userIsLoggedIn = false;
+
+// Route for the landing page
+app.get('/', (req, res) => {
+    // Render the landing page, passing the userIsLoggedIn variable to the header partial
+    res.render('landing', { userIsLoggedIn });
+});
+
+// Other routes for pricing, features, etc. can also be added here
+// ...
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,23 +1,7 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = 3000;
-
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-
-// Set the views directory
-app.set('views', path.join(__dirname, 'src', 'views'));
-
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Define a route for the landing page
-app.get('/', (req, res) => {
-    res.render('landing');
-});
+const app = require('./app'); // Import the app.js file
 
 // Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
