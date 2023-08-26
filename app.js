38ddab45ed2c3a36fa711bc14401bbe1bb02d886
Robert Stenulson <61328772+RobertStenulson@users.noreply.github.com>
2023-08-26 19:14:29 -0300

    added passport

--- app.js
+++ app.js
@@ -1,28 +1,127 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const dotenv = require('dotenv');
+const flash = require('connect-flash');
+const User = require('./src/models/user');
+const ejsMate = require('ejs-mate')
+const nodemailer = require('nodemailer');
+
+
+dotenv.config(); // Load environment variables from .env file
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database:', err.message);
+    });
 
 // Set up the static middleware to serve files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set the view engine and views folder
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
-// Simulated user authentication status
-let userIsLoggedIn = false;
+app.use(flash());
+
+
+// Initialize and configure passport
+app.use(session({
+    secret: 'your-secret-key', // Change this to a strong secret key
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// pass 'userIsLoggedIn' to header
+app.use((req, res, next) => {
+    res.locals.userIsLoggedIn = req.isAuthenticated();
+    next();
+});
+
+// Parse URL-encoded bodies (for form data)
+app.use(express.urlencoded({ extended: false }));
+
+// Serialize and deserialize user
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then((user) => {
+            done(null, user);
+        })
+        .catch((err) => {
+            done(err, null);
+        });
+});
+
+
+// // Use the local strategy for authentication
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+},
+    (email, password, done) => {
+        User.findOne({ email: email }, (err, user) => {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false, { message: 'Incorrect email.' }); }
+            if (!user.verifyPassword(password)) { return done(null, false, { message: 'Incorrect password.' }); }
+            return done(null, user);
+        });
+    }
+));
+
+
+// test
+// passport.use(new LocalStrategy({
+//     usernameField: 'email',
+// }, (email, password, done) => {
+//     const user = { id: 1, email: 'testuser@email.com', password: 'testpassword' };
+
+//     // Check if the email exists in the user object and if the password matches
+//     if (email !== user.email || password !== user.password) {
+//         return done(null, false, { message: 'Incorrect email or password.' });
+//     }
+
+//     // If both email and password match, authentication is successful
+//     return done(null, user);
+// }));
+
+// Import the routes from "index.js"
+const routes = require('./src/routes/index');
+app.use('/', routes);
 
 // Require and use the auth routes
 const authRoutes = require('./src/routes/auth');
+const { log } = require('console');
 app.use(authRoutes);
 
-// Route for the landing page
-app.get('/', (req, res) => {
-    // Render the landing page, passing the userIsLoggedIn variable to the header partial
-    res.render('landing', { userIsLoggedIn });
+
+// Configure nodemailer transporter for sending emails
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+    secure: true, // Use SSL/TLS
 });
 
-// Other routes for pricing, features, etc. can also be added here
-// ...
+module.exports = { app, transporter };
 
 module.exports = app;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,20 +1,187 @@
-// routes/auth.js
-
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const crypto = require('crypto'); // For generating random tokens
+const nodemailer = require('nodemailer'); // For sending emails
+const { transporter } = require('../../app'); // Require the transporter from app.js
+const flash = require('connect-flash');
 
-// Simulated user authentication status
-let userIsLoggedIn = false;
 
-// Define routes
+
+
+// GET login page
 router.get('/login', (req, res) => {
-    res.render('auth/login', { userIsLoggedIn });
+    res.render('auth/login', { title: 'Login' });
 });
 
+// POST login page
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/dashboard', // Change this to the page you want to redirect after successful login
+    failureRedirect: '/login', // Change this to the login page or display an error message
+}));
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+}, async (email, password, done) => {
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return done(null, false, { message: 'Incorrect email.' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+}));
+
+//In this updated code, we use bcrypt.compare to compare the entered password with the hashed password from the database. If the passwords match, authentication is successful, and the user is redirected to the /dashboard page. Otherwise, an error message is displayed or redirected back to the login page.
+
+
+// GET signup page
 router.get('/signup', (req, res) => {
-    res.render('auth/signup', { userIsLoggedIn });
+    res.render('auth/signup', { title: 'Sign Up' });
 });
 
-// Add more routes as needed for the authentication process
+// POST signup page
+router.post('/signup', async (req, res) => {
+    const { email, password, confirmPassword } = req.body;
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+        return res.render('auth/signup', { title: 'Sign Up', error: 'Passwords do not match.' });
+    }
+
+    try {
+        // Check if the email already exists in the database
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+            return res.render('auth/signup', { title: 'Sign Up', error: 'Email already registered.' });
+        }
+
+        // Hash the password before saving it to the database
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        // Create the new user and save it to the database
+
+        const user = new User({ email: req.body.email, password: hashedPassword });
+        await user.save();
+
+        res.redirect('/login'); // Redirect to the login page after successful signup
+    } catch (err) {
+        console.error(err);
+        res.render('auth/signup', { title: 'Sign Up', error: 'An error occurred during signup.' });
+    }
+});
+
+// GET route for password reset request
+router.get('/forgot-password', (req, res) => {
+    res.render('auth/forgot-password', { title: 'Forgot Password' });
+});
+
+// POST route for password reset request
+router.post('/forgot-password', async (req, res) => {
+    try {
+        // Find the user by their email
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            req.flash('error', 'No user found with that email address.');
+            return res.redirect('/forgot-password');
+        }
+
+        // Generate a reset token and expiration date
+        const token = crypto.randomBytes(32).toString('hex');
+        const tokenExpiry = Date.now() + 3600000; // 1 hour
+
+        // Store the token and expiry in the user's record
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = tokenExpiry;
+        await user.save();
+
+        // Send the password reset email
+        const transporter = nodemailer.createTransport({
+            // Set up your nodemailer transporter configuration here
+        });
+
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: user.email,
+            subject: 'Password Reset Request',
+            html: `Click <a href="http://localhost:3000/reset-password/${token}">here</a> to reset your password.`
+        });
+
+        req.flash('success', 'Password reset email sent.');
+        res.redirect('/forgot-password');
+    } catch (err) {
+        console.error(err);
+        req.flash('error', 'An error occurred while processing your request.');
+        res.redirect('/forgot-password');
+    }
+});
+
+// GET route for reset password form
+router.get('/reset-password/:token', (req, res) => {
+    res.render('auth/reset-password', { title: 'Reset Password', token: req.params.token });
+});
+
+// POST route for resetting password
+router.post('/reset-password/:token', async (req, res) => {
+    try {
+        const token = req.params.token;
+
+        // Find the user by their reset token and check if it's still valid
+        const user = await User.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpires: { $gt: Date.now() },
+        });
+
+        if (!user) {
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('/forgot-password');
+        }
+
+        // Check if the new password matches the confirmation password
+        if (req.body.password !== req.body.confirmPassword) {
+            req.flash('error', 'Passwords do not match.');
+            return res.redirect(`/reset-password/${token}`);
+        }
+
+        // Update the user's password and clear the reset token fields
+        user.password = req.body.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        await user.save();
+
+        req.flash('success', 'Password has been reset successfully.');
+        res.redirect('/login');
+    } catch (err) {
+        console.error(err);
+        req.flash('error', 'An error occurred while processing your request.');
+        res.redirect(`/reset-password/${token}`);
+    }
+});
+
+// GET logout page
+router.get('/logout', (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            // Handle any error that occurred during logout
+            console.error(err);
+        }
+        // Redirect the user to the home page after logout
+        res.redirect('/');
+    });
+});
+
+
+
 
 module.exports = router;
