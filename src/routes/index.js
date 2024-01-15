b5fec1cc9362dfca726ea232aef4cd6c51e0ccdf
Robert Stenulson <61328772+RobertStenulson@users.noreply.github.com>
2024-01-15 19:53:50 -0300

    Add estimate routes and controller functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const { ensureAuthenticated } = require('../../middleware/auth');
+const estimateController = require('../controllers/estimateController');
 
 
 // Landing page route
@@ -15,8 +16,22 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
 });
 
 // GET route for create estimate form
-router.get('/create-estimate', (req, res) => {
+router.get('/create-estimate', ensureAuthenticated, (req, res) => {
     res.render('create-estimate', { title: 'Create Estimate' });
 });
+
+// POST route to submit the create estimate form
+router.post('/create-estimate', ensureAuthenticated, estimateController.submitEstimate);
+
+router.get('/estimates', ensureAuthenticated, estimateController.displayEstimates);
+
+router.delete('/estimates/:id', ensureAuthenticated, estimateController.deleteEstimate);
+
+router.get('/estimates/:id', ensureAuthenticated, estimateController.viewEstimate);
+
+// Edit Estimate Form
+router.get('/estimates/:id/edit', ensureAuthenticated, estimateController.editEstimate);
+
+router.post('/estimates/:id/edit', ensureAuthenticated, estimateController.updateEstimate);
 
 module.exports = router;
